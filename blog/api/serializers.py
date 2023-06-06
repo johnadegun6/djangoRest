063c0b5a05149266fd510ebd6f7abc4d748e651c
johnadegun6 <125451917+johnadegun6@users.noreply.github.com>
2023-06-06 15:09:08 +0100

    djangoRest

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    # title = serializers.CharField(max_length = 255)
+    # content = serializers.CharField
+    # created_at = serializers.DateTimeField()
+
+    # def create(self, validated_data):
+    #     return Post.objects.create
 
     class Meta:
 
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from. import views
 
 urlpatterns = [
-    re_path(r'^posts/$', views.posts, name= 'posts')
+    re_path(r'^posts/$', views.BlogPost.as_view(), name= 'posts'),
     #re_path(r'^posts/?p<pk>)
 ]
